eee222967b4011cf2f66320cc9b6859d11be84cb
mikehailu7 <mikehailu7@gmail.com>
2024-03-31 23:44:58 -0700

    task 0

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,6 @@
+// Author: MikiasHailu
+import $ from 'jquery';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
