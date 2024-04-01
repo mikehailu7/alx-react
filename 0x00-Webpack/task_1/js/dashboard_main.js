4253d6d664977a733bc5a5c5976c6984539a45d9
mikehailu7 <mikehailu7@gmail.com>
2024-04-01 10:14:20 +0300

    task 1

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,18 @@
+// Author: MikiasHailu
+import $ from 'jquery';
+import _ from 'lodash';
+
+$('body').append(`<p>Holberton Dashboard</p>`);
+$('body').append(`<p>Dashboard data for the students</p>`);
+$('body').append(`<button>Click here to get started</button>`);
+$('body').append(`<p id="count"></p>`);
+$('body').append(`<p>Copyright - Holberton School</p>`);
+
+$('button').on('click', _.debounce(updateCounter, 500));
+
+let count  = 0;
+// This function update counter
+function updateCounter() {
+	count++;
+	$('#count').text(`${count} clicks on the button`);
+}
