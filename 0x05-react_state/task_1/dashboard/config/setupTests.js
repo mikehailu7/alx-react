f519d249791ed583455fd52f92dd71a4b6a2bb2d
mikehailu7 <mikehailu7@gmail.com>
2024-04-17 09:09:37 +0300

    react state

---
+++ 0x05-react_state/task_1/dashboard/config/setupTests.js
@@ -0,0 +1,6 @@
+//Author: Mikias Hailu
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+
+configure({ adapter: new Adapter() });
---
+++ 0x05-react_state/task_3/dashboard/src/Notifications/NotificationItemShape.js
@@ -0,0 +1,9 @@
+//Author: Mikias Hailu
+import PropType from 'prop-types';
+
+export default PropType.shape({
+  id: PropType.number.isRequired,
+  html: PropType.shape({__html: PropType.string}),
+  type: PropType.string.isRequired,
+  value: PropType.string
+});
