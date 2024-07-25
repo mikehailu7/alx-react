f474162af6dd4f5f5e6d94ced8d8829416805bff
mikehailu7 <mikehailu7@gmail.com>
2024-07-25 09:35:51 +0300

    react_redux_reducer_selector

---
+++ 0x08-react_redux_reducer_selector/task_5/dashboard/src/CourseList/CourseShape.js
@@ -0,0 +1,11 @@
+//Author:Mikias Hailu
+import PropTypes from 'prop-types';
+
+
+const CourseShape = PropTypes.shape({
+  id: PropTypes.number,
+  name: PropTypes.string,
+  credit: PropTypes.number,
+});
+
+export default CourseShape;
